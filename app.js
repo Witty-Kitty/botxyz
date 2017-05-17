246ef1b3692ee5c169ee832370057ccdeb83350b
kathleen <kathleensiminyu@gmail.com>
2017-05-17 16:38:33 +0300

    new functionality

--- app.js
+++ app.js
@@ -21,9 +21,37 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
-
+//var bot = new builder.UniversalBot(connector, function (session) {
+ //   session.send("You said: %s", session.message.text);
+//});
 
 server.get(/.*/, restify.serveStatic({ 'directory': '.', 'default': 'index.html' }));
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Hi... We sell shirts. Say 'show shirts' to see our products.");
+});
+
+// Add dialog to return list of shirts available
+bot.dialog('showShirts', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Classic White T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+            ]),
+        new builder.HeroCard(session)
+            .title("Classic Gray T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+            ])
+    ]);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: /^(show|list)/i });
