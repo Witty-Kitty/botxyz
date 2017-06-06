d022728b6fb1f74fde2fbcd6f0315b66320c1970
kathleen <kathleensiminyu@gmail.com>
2017-06-06 16:46:19 +0300

    added url

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
     // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
     var bot = new builder.UniversalBot(connector, function (session) {
         session.send("You said: %s", session.message.text);
+        session.send("http://7e92e786.ngrok.io/");
     });
 
     server.get(/.*/, restify.serveStatic({ 'directory': '.', 'default': 'index.html' }));
